304303f0f2b880d905facc0be1154546de9768cd
njucs <njucs@os.com>
2022-02-17 19:30:13 +0800

    first commit in main

---
+++ bootloader/start.s
@@ -0,0 +1,25 @@
+
+/* Protected Mode Hello World */
+#.code16
+#
+#.global start
+#start:
+#	movw %cs, %ax
+#	movw %ax, %ds
+#	movw %ax, %es
+#	movw %ax, %ss
+#
+#.code32
+#start32:
+#	movw $0x10, %ax # setting data segment selector
+#	movw %ax, %ds
+#	movw %ax, %es
+#	movw %ax, %fs
+#	movw %ax, %ss
+#loop32:
+#	jmp loop32
+#
+#
+#.p2align 2
+
+
